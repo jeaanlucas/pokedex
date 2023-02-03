de88859a32f7482c4ac174bfa4c8b688c58be907
Jean Lucas de Camillo <jeanl.camillo@gmail.com>
2023-02-03 14:22:51 -0300

    Criada estrutura para a divisão do projeto em multifrontend

---
+++ features/commons_dependencies/lib/main.dart
@@ -0,0 +1,7 @@
+export 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+export 'package:image_picker/image_picker.dart';
+export 'package:shared_preferences/shared_preferences.dart';
+export 'package:uuid/uuid.dart';
+export 'package:dio/dio.dart';
+export 'package:flutter_mobx/flutter_mobx.dart';
+export 'package:flutter_modular/flutter_modular.dart';
